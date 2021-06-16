8f8da2ba851fe42dd30345da4d3bc2cc2a54f32b
Liempo <gonzalesalec@gmail.com>
2021-06-16 23:42:41 +0800

    Add firebase auth repository

---
+++ lib/repositories/auth_repository.dart
@@ -0,0 +1,36 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class AuthRepository extends ChangeNotifier {
+
+  // Save the firebase auth instance
+  var _auth = FirebaseAuth.instance;
+
+  // Create account using email and password
+  Future register({
+    required String email,
+    required String password
+  }) async {
+    try {
+      await _auth.createUserWithEmailAndPassword(
+        email: email, password: password);
+    } catch (e) { print(e); }
+  }
+
+  // Authenticate using email and password
+  Future login({
+    required String email,
+    required String password
+  }) async {
+    try {
+      // Send login information to firebase
+      await _auth.signInWithEmailAndPassword(
+        email: email, password: password);
+    } catch (e) { print(e); }
+  }
+
+  Future logout() async {
+    _auth.signOut();
+  }
+
+}
