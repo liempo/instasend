a094231ebdd5b77ca413b1a077790d9b3c108ba3
Liempo <gonzalesalec@gmail.com>
2021-06-17 18:45:24 +0800

    Move initialization to SplashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 import '/utils/constants.dart';
-import '/ui/auth/auth_screen.dart';
 import '/ui/splash/splash_screen.dart';
 
 void main() {
@@ -18,26 +16,12 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
 
-  // Firebase future initialization
-  final Future<FirebaseApp> _initialization
-    = Firebase.initializeApp();
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Instasend',
       theme: lightTheme,
-      home: FutureBuilder(
-        future: _initialization,
-        builder: (context, snapshot) {
-          // Get firebase's connection state
-          final state = snapshot.connectionState;
-          // Decide what screen to show based on state
-          if (state == ConnectionState.done)
-            return AuthScreen.withViewModel();
-          return SplashScreen();
-        },
-      )
+      home: SplashScreen()
     );
   }
 
--- lib/ui/splash/splash_screen.dart
+++ lib/ui/splash/splash_screen.dart
@@ -1,21 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
+import '/ui/auth/auth_screen.dart';
 import '/utils/image_extension.dart';
 
 class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Image.asset(
-          'assets/logos/circle.png',
-          height: 128, width: 128
-        ).withTint(
-          color: Theme.of(context)
-            .primaryColor
-        ),
-      )
+    return FutureBuilder(
+      future: Firebase.initializeApp(),
+      builder: (context, snapshot) {
+        // Get firebase's connection state
+        final state = snapshot.connectionState;
+        // Decide what screen to show based on state
+        if (state == ConnectionState.done)
+          return AuthScreen.withViewModel();
+        // Show splash screen while firebase
+        // connection is not yet established
+        return Scaffold(
+          backgroundColor: Theme.of(context)
+            .accentColor,
+          body: Center(
+            child: Image.asset(
+              'assets/logos/circle.png',
+              height: 128, width: 128
+            ).withTint(
+              color: Theme.of(context)
+                .primaryColor
+            ),
+          )
+        );
+      },
     );
   }
+
 }
