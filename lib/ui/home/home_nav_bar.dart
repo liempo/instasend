225cb2b9baba48c071d093b6a112d79854a57197
Liempo <gonzalesalec@gmail.com>
2021-06-27 15:45:46 +0800

    Add navBar and appBar to HomeScreen

---
+++ lib/ui/home/home_nav_bar.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class HomeNavBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(0, -8),
+            blurRadius: 24,
+            color: Theme.of(context)
+              .primaryColor
+              .withOpacity(0.15)
+          )
+        ]
+      ),
+      child: BottomNavigationBar(
+        elevation: 0,
+        items: [
+          BottomNavigationBarItem(
+            label: "Shop",
+            icon: Icon(Icons.store),
+          ),
+          BottomNavigationBarItem(
+            label: "Delivery",
+            icon: Icon(Icons.delivery_dining),
+          ),
+          BottomNavigationBarItem(
+            label: "Profile",
+            icon: Icon(Icons.person),
+          ),
+        ]
+      ),
+    );
+  }
+}
